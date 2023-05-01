a821ff106a7dbafa3304e40e5b51aba2e6e65d67
Katarzyna Juchniewicz <juchkasia@gmail.com>
2023-05-01 19:51:44 +0200

    feat: add array keys, MON 01.05.2023 19.50

--- virtual keyboard/src/index.js
+++ virtual keyboard/src/index.js
@@ -86,21 +86,630 @@ keyboard.append(keyboardRow5);
 
 
 // Создаем кнопки клавиатуры
-const keysEng = [
-  '`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'backspace',
-  'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'DEL',
-  'Caps Lock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'enter', 
-  'shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '▲', 'shift',
-  'Ctrl', 'Win', 'alt', 'space', 'alt', 'Ctrl', '◄', '▼', '►'
-];
+// const keysEng = [
+//   '`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'backspace',
+//   'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'DEL',
+//   'Caps Lock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'enter', 
+//   'shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '▲', 'shift',
+//   'Ctrl', 'Win', 'alt', 'space', 'alt', 'Ctrl', '◄', '▼', '►'
+// ];
 
-const keysRu = [
-  'ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'backspace',
-  'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\', 'DEL',
-  'Caps Lock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'enter', 
-  'shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', 'ю', '.', '▲', 'shift',
-  'Ctrl', 'Win', 'alt', 'space', 'alt', 'Ctrl', '◄', '▼', '►'
-];
+// const keysRu = [
+//   'ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'backspace',
+//   'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\', 'DEL',
+//   'Caps Lock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'enter', 
+//   'shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', 'ю', '.', '▲', 'shift',
+//   'Ctrl', 'Win', 'alt', 'space', 'alt', 'Ctrl', '◄', '▼', '►'
+// ];
+
+const keys = [
+  {
+    key: {
+      ru: "ё",
+      en: "`"
+    },
+    shift: {
+        ru: "Ё",
+        en: "~"
+    },
+    code: "Backquote"
+  }, {
+    key: {
+        ru: "1",
+        en: "1"
+    },
+    shift: {
+        ru: "!",
+        en: "!"
+    },
+    code: "Digit1"
+  }, {
+    key: {
+        ru: "2",
+        en: "2"
+    },
+    shift: {
+        ru: '"',
+        en: "@"
+    },
+    code: "Digit2"
+  }, {
+    key: {
+        ru: "3",
+        en: "3"
+    },
+    shift: {
+        ru: "№",
+        en: "#"
+    },
+    code: "Digit3"
+  }, {
+    key: {
+        ru: "4",
+        en: "4"
+    },
+    shift: {
+        ru: ";",
+        en: "$"
+    },
+    code: "Digit4"
+  }, {
+    key: {
+        ru: "5",
+        en: "5"
+    },
+    shift: {
+        ru: "%",
+        en: "%"
+    },
+    code: "Digit5"
+  }, {
+    key: {
+        ru: "6",
+        en: "6"
+    },
+    shift: {
+        ru: ":",
+        en: "^"
+    },
+    code: "Digit6"
+  }, {
+    key: {
+        ru: "7",
+        en: "7"
+    },
+    shift: {
+        ru: "?",
+        en: "&"
+    },
+    code: "Digit7"
+  }, {
+    key: {
+        ru: "8",
+        en: "8"
+    },
+    shift: {
+        ru: "*",
+        en: "*"
+    },
+    code: "Digit8"
+  }, {
+    key: {
+        ru: "9",
+        en: "9"
+    },
+    shift: {
+        ru: "(",
+        en: "("
+    },
+    code: "Digit9"
+  }, {
+    key: {
+        ru: "0",
+        en: "0"
+    },
+    shift: {
+        ru: ")",
+        en: ")"
+    },
+    code: "Digit0"
+  }, {
+    key: {
+        ru: "-",
+        en: "-"
+    },
+    shift: {
+        ru: "_",
+        en: "_"
+    },
+    code: "Minus"
+  }, {
+    key: {
+        ru: "=",
+        en: "="
+    },
+    shift: {
+        ru: "+",
+        en: "+"
+    },
+    code: "Equal"
+  }, {
+    key: {
+      ru: "Backspace",
+      en: "Backspace"
+    },
+    code: "Backspace",
+    // class: "key-backspace",
+    // noType: !0
+  }, 
+  {
+    key: {
+      ru: "Tab",
+      en: "Tab"
+    },
+    code: "Tab",
+    // class: "key-tab",
+    // noType: !0
+  }, {
+    key: {
+        ru: "й",
+        en: "q"
+    },
+    shift: {
+        ru: "Й",
+        en: "Q"
+    },
+    code: "KeyQ"
+  }, {
+    key: {
+        ru: "ц",
+        en: "w"
+    },
+    shift: {
+        ru: "Ц",
+        en: "W"
+    },
+    code: "KeyW"
+  }, {
+    key: {
+        ru: "у",
+        en: "e"
+    },
+    shift: {
+        ru: "У",
+        en: "E"
+    },
+    code: "KeyE"
+  }, {
+    key: {
+        ru: "к",
+        en: "r"
+    },
+    shift: {
+        ru: "К",
+        en: "R"
+    },
+    code: "KeyR"
+  }, {
+    key: {
+        ru: "е",
+        en: "t"
+    },
+    shift: {
+        ru: "Е",
+        en: "T"
+    },
+    code: "KeyT"
+  }, {
+    key: {
+        ru: "н",
+        en: "y"
+    },
+    shift: {
+        ru: "Н",
+        en: "Y"
+    },
+    code: "KeyY"
+  }, {
+    key: {
+        ru: "г",
+        en: "u"
+    },
+    shift: {
+        ru: "Г",
+        en: "U"
+    },
+    code: "KeyU"
+  }, {
+    key: {
+        ru: "ш",
+        en: "i"
+    },
+    shift: {
+        ru: "Ш",
+        en: "I"
+    },
+    code: "KeyI"
+  }, {
+    key: {
+        ru: "щ",
+        en: "o"
+    },
+    shift: {
+        ru: "Щ",
+        en: "O"
+    },
+    code: "KeyO"
+  }, {
+    key: {
+        ru: "з",
+        en: "p"
+    },
+    shift: {
+        ru: "З",
+        en: "P"
+    },
+    code: "KeyP"
+  }, {
+    key: {
+        ru: "х",
+        en: "["
+    },
+    shift: {
+        ru: "Х",
+        en: "{"
+    },
+    code: "BracketLeft"
+  }, {
+    key: {
+        ru: "ъ",
+        en: "]"
+    },
+    shift: {
+        ru: "Ъ",
+        en: "}"
+    },
+    code: "BracketRight"
+  }, {
+    key: {
+        ru: "\\",
+        en: "\\"
+    },
+    shift: {
+        ru: "|",
+        en: "|"
+    },
+    code: "Backslash",
+    // class: "key-backslash"
+  }, {
+    key: {
+      ru: "Del",
+      en: "Del"
+    },
+    code: "Delete",
+    // class: "key-del",
+    // noType: !0
+  },
+  {
+    key: {
+      ru: "CapsLock",
+      en: "CapsLock"
+    },
+    code: "CapsLock",
+    // class: "key-capslock",
+    // noType: !0
+  }, {
+    key: {
+      ru: "ф",
+      en: "a"
+    },
+    shift: {
+        ru: "Ф",
+        en: "A"
+    },
+    code: "KeyA"
+  }, {
+    key: {
+        ru: "ы",
+        en: "s"
+    },
+    shift: {
+        ru: "Ы",
+        en: "S"
+    },
+    code: "KeyS"
+  }, {
+    key: {
+        ru: "в",
+        en: "d"
+    },
+    shift: {
+        ru: "В",
+        en: "D"
+    },
+    code: "KeyD"
+  }, {
+    key: {
+        ru: "а",
+        en: "f"
+    },
+    shift: {
+        ru: "А",
+        en: "F"
+    },
+    code: "KeyF"
+  }, {
+    key: {
+        ru: "п",
+        en: "g"
+    },
+    shift: {
+        ru: "П",
+        en: "G"
+    },
+    code: "KeyG"
+  }, {
+    key: {
+        ru: "р",
+        en: "h"
+    },
+    shift: {
+        ru: "Р",
+        en: "H"
+    },
+    code: "KeyH"
+  }, {
+    key: {
+        ru: "о",
+        en: "j"
+    },
+    shift: {
+        ru: "О",
+        en: "J"
+    },
+    code: "KeyJ"
+  }, {
+    key: {
+        ru: "л",
+        en: "k"
+    },
+    shift: {
+        ru: "Л",
+        en: "K"
+    },
+    code: "KeyK"
+  }, {
+    key: {
+        ru: "д",
+        en: "l"
+    },
+    shift: {
+        ru: "Д",
+        en: "L"
+    },
+    code: "KeyL"
+  }, {
+    key: {
+        ru: "ж",
+        en: ";"
+    },
+    shift: {
+        ru: "Ж",
+        en: ":"
+    },
+    code: "Semicolon"
+  }, {
+    key: {
+        ru: "э",
+        en: "'"
+    },
+    shift: {
+        ru: "Э",
+        en: '"'
+    },
+    code: "Quote"
+  }, {
+    key: {
+      ru: "Enter",
+      en: "Enter"
+    },
+    code: "Enter",
+    // class: "key-enter",
+    // noType: !0
+  }, {
+    key: {
+      ru: "Shift",
+      en: "Shift"
+    },
+    code: "ShiftLeft",
+    // class: "key-leftshift",
+    // noType: !0
+  }, {
+    key: {
+        ru: "я",
+        en: "z"
+    },
+    shift: {
+        ru: "Я",
+        en: "Z"
+    },
+    code: "KeyZ"
+  }, {
+    key: {
+        ru: "ч",
+        en: "x"
+    },
+    shift: {
+        ru: "Ч",
+        en: "X"
+    },
+    code: "KeyX"
+  }, {
+    key: {
+        ru: "с",
+        en: "c"
+    },
+    shift: {
+        ru: "С",
+        en: "C"
+    },
+    code: "KeyC"
+  }, {
+    key: {
+        ru: "м",
+        en: "v"
+    },
+    shift: {
+        ru: "М",
+        en: "V"
+    },
+    code: "KeyV"
+  }, {
+    key: {
+        ru: "и",
+        en: "b"
+    },
+    shift: {
+        ru: "И",
+        en: "B"
+    },
+    code: "KeyB"
+  }, {
+    key: {
+        ru: "т",
+        en: "n"
+    },
+    shift: {
+        ru: "Т",
+        en: "N"
+    },
+    code: "KeyN"
+  }, {
+    key: {
+        ru: "ь",
+        en: "m"
+    },
+    shift: {
+        ru: "Ь",
+        en: "M"
+    },
+    code: "KeyM"
+  }, {
+    key: {
+        ru: "б",
+        en: ","
+    },
+    shift: {
+        ru: "Б",
+        en: "<"
+    },
+    code: "Comma"
+  }, {
+    key: {
+        ru: "ю",
+        en: "."
+    },
+    shift: {
+        ru: "Ю",
+        en: ">"
+    },
+    code: "Period"
+  }, {
+    key: {
+        ru: ".",
+        en: "/"
+    },
+    shift: {
+        ru: ",",
+        en: "?"
+    },
+    code: "Slash"
+  }, {
+    key: {
+      ru: "▲",
+      en: "▲",
+    },
+    code: "ArrowUp",
+    // noType: !0
+  }, {
+    key: {
+      ru: "Shift",
+      en: "Shift",
+    },
+    code: "ShiftRight",
+    // class: "key-rightshift",
+    // noType: !0
+  }, {
+    key: {
+      ru: "Ctrl",
+      en: "Ctrl",
+    },
+    code: "ControlLeft",
+    // class: "key-leftctrl",
+    // noType: !0
+  }, {
+    key: {
+      ru: "Win",
+      en: "Win",
+    },
+    code: "MetaLef",
+    // class: "key-leftalt",
+    // noType: !0
+  }, {
+    key: {
+      ru: "Alt",
+      en: "Alt",
+    },
+    code: "AltLeft",
+    // class: "key-leftalt",
+    // noType: !0
+  }, {
+    key: {
+      ru: "Space",
+      en: "Space",
+    },
+    code: "Space",
+    // class: "key-space"
+  }, {
+    key: {
+      ru: "Alt",
+      en: "Alt",
+    },
+    code: "AltRight",
+    // class: "key-rightalt",
+    // noType: !0
+  }, {
+    key: {
+      ru: "Ctrl",
+      en: "Ctrl",
+    },
+    code: "ControlRight",
+    // class: "key-rightctrl",
+    // noType: !0
+  }, {
+    key: {
+      ru: "◄",
+      en: "◄",
+    },
+    code: "ArrowLeft",
+    // noType: !0
+  }, {
+    key: {
+      ru: "▼",
+      en: "▼",
+    },
+    code: "ArrowDown",
+    // noType: !0
+  }, {
+    key: {
+      ru: "►",
+      en: "►",
+    },
+    code: "ArrowRight",
+    // noType: !0
+  },
+]
+
 
 // Добавляем каждую кнопку в контейнер кнопок ENGLISH LANGUAGE
 function showKeys(keys) {
